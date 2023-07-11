7a953d41255aa96ff1cc0d101842a1565a0d849e
Silvestro Frisullo <39952643+molakp@users.noreply.github.com>
2023-07-11 21:59:56 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from helpers.scraper import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    board_name='pol'
+    board_url = 'https://boards.4chan.org/' + board_name
+    threads = get_threads(board_url)
+    print(threads)  # Stampa la lista dei thread per debug
+    return render_template('index.html', threads=threads)
+
+
+@app.route('/thread/<thread_id>')
+def thread(thread_id):
+    # Code to retrieve thread details from thread_id
+    # Assume the thread is represented as a Python object with attributes like title, posts, etc.
+    thread = get_thread_details(thread_id)
+    return render_template('thread.html', thread=thread)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ helpers/scraper.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_soup(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup
+
+def get_threads(board_url):
+    soup = get_soup(board_url)
+    thread_divs = soup.select('div.thread')
+    threads = []
+
+    for div in thread_divs:
+        thread = {}
+        title_div = div.select_one('.postContainer.opContainer .postInfo.desktop .subject')
+
+        if title_div:
+            thread['title'] = title_div.text.strip()
+        link = div.select_one('a[href^="//boards.4chan.org"]')
+        if link:
+            thread['url'] = link.get('href')
+            thread['title'] = link.text.strip()
+
+        thumb = div.select_one('.thumb')
+        if thumb and 'src' in thumb.attrs:
+            thread['image_url'] = thumb['src']
+
+        meta = div.select_one('.meta')
+        if meta:
+            reply_count = meta.select_one('b')
+            if reply_count:
+                thread['reply_count'] = reply_count.text.strip()
+
+        teaser = div.select_one('.teaser')
+        if teaser:
+            thread['teaser'] = teaser.text.strip()
+
+
+        thread['id'] = div['id'][1:]  # Remove the leading 't' from the ID
+        message =  div.select_one('.postContainer.opContainer .postMessage')
+        if message:
+            thread['message'] = message.text.strip()
+       
+
+
+        threads.append(thread)
+
+    return threads
+
+
+
+def get_thread_details(board_url, thread_id):
+    thread_url = f'{board_url}/thread/{thread_id}'
+    soup = get_soup(thread_url)
+    thread_details = {}
+    thread_details['id'] = thread_id
+    
+    thread_details['title'] = soup.find('span', class_='subject').get_text(strip=True)
+    post_message_div = soup.find('div', class_='postMessage')
+    thread_details['content'] = post_message_div.get_text(strip=True) if post_message_div else ''
+    return thread_details
+
